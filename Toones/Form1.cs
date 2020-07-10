581b3abb41849b689df58e76b4e21a46bc2f20e8
Wubzy <peytonthegreat0@gmail.com>
2020-07-10 15:18:22 -0400

    Songs Update

--- Toones/Form1.cs
+++ Toones/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Toones
 {
@@ -17,14 +18,47 @@ namespace Toones
 			InitializeComponent();
 		}
 
+		public string thisDirectory = Path.Combine(Environment.CurrentDirectory);
+		private string selectedSong;
+
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			
+			songList.Items.Clear();
+			DirectoryInfo dinfo = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, @"Songs\"));
+			FileInfo[] smFiles = dinfo.GetFiles("*.mp3");
+			foreach (FileInfo fi in smFiles)
+			{
+				songList.Items.Add(Path.GetFileNameWithoutExtension(fi.Name));
+			}
 		}
 
 		private void label1_Click(object sender, EventArgs e)
 		{
 
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			axWindowsMediaPlayer1.Ctlcontrols.stop();
+			string filePath = Path.Combine(Environment.CurrentDirectory, @"Songs\", selectedSong + ".mp3");
+			axWindowsMediaPlayer1.URL = filePath;
+			axWindowsMediaPlayer1.Ctlcontrols.play();
+			label2.Text = selectedSong;
+		}
+
+		private void songList_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			selectedSong = songList.SelectedItem.ToString();
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox2_Click(object sender, EventArgs e)
+		{
+			axWindowsMediaPlayer1.Ctlcontrols.stop();
 		}
 	}
 }
