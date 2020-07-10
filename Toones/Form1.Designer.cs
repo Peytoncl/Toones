19f22f46208dfea2d4036c9168e43db2d94a3824
Wubzy <peytonthegreat0@gmail.com>
2020-07-10 13:25:13 -0400

    Panel

--- Toones/Form1.Designer.cs
+++ Toones/Form1.Designer.cs
@@ -28,13 +28,35 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+			this.webBrowser1.TabIndex = 0;
+			this.webBrowser1.Url = new System.Uri("https://open.spotify.com/track/615vw74T1NebV8pKoutm2J?si=bQX_x9XDTqOZbH06hqVDiQ", System.UriKind.Absolute);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.webBrowser1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.WebBrowser webBrowser1;
 	}
 }
 
--- Toones/Form1.cs
+++ Toones/Form1.cs
@@ -16,5 +16,15 @@ namespace Toones
 		{
 			InitializeComponent();
 		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+
+		}
 	}
 }
